2ceff506bfa2aa815cb7798cc7a64974f53d7c14
liubo <>
2021-06-01 22:00:04 +0800

    添加新视图

---
+++ LXBRollingView/Classes/KKView.m
@@ -0,0 +1,20 @@
+//
+//  KKView.m
+//  LXBRollingView
+//
+//  Created by liubo on 2021/6/1.
+//
+
+#import "KKView.h"
+
+@implementation KKView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
